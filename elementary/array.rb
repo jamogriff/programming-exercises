443c73f76272551d476e6bfa632678d361275447
Jamison Griffith <jamison.griffith@gmail.com>
2020-12-31 13:43:59 -0700

    Adds array file to starts primes algorithm

---
+++ elementary/array.rb
@@ -0,0 +1,24 @@
+
+=begin
+Getting started with arrays to implement Sieve of Eratosthenes... 
+
+puts "Enter the upper limit to find all of the prime numbers less than it: "
+userInput = gets.chomp
+userInt = Integer(userInput)
+=end
+
+num = 100
+array = Array(1..num)
+
+for item in array
+  if item.even?
+    
+  end
+end
+=begin
+while num > 2 do
+  if num.is_even?
+    num -= 1
+  
+  else
+=end
